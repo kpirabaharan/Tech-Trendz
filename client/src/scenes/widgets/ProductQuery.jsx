7732d34bacc9fa679dc96e646891c3ae8642f38e
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-03-18 20:49:03 -0400

    Client: Added Product Query Highlighting
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- client/src/scenes/widgets/ProductQuery.jsx
+++ client/src/scenes/widgets/ProductQuery.jsx
@@ -1,27 +1,38 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const ProductQuery = () => {
+  const [mode, setMode] = useState('all');
+
   return (
     <div className='flex justify-center gap-8 font-playfair'>
       <Link
-        className='text-center py-2 border-gray-400 hover:bg-gray-200 border-2 w-[100px] rounded-lg'
-        to={'?mode=all'}
+        className={`
+          ${mode == 'all' ? 'bg-blue-300' : 'hover:bg-gray-200'}
+        text-center py-2 border-gray-400  border-2 w-[100px] rounded-lg`}
+        to={'/?mode=all'}
+        onClick={() => setMode('all')}
       >
         All
       </Link>
       <Link
-        className='text-center py-2 border-gray-400 hover:bg-gray-200 border-2 w-[100px] rounded-lg'
-        to={'?mode=new'}
+        className={`
+          ${mode == 'new' ? 'bg-blue-300' : 'hover:bg-gray-200'}
+        text-center py-2 border-gray-400  border-2 w-[100px] rounded-lg`}
+        to={'/?mode=new'}
+        onClick={() => setMode('new')}
       >
         New
       </Link>
       <Link
-        className='text-center py-2 border-gray-400 hover:bg-gray-200 border-2 w-[100px] rounded-lg'
-        to={'?mode=top'}
+        className={`
+          ${mode == 'top' ? 'bg-blue-300' : 'hover:bg-gray-200'}
+        text-center py-2 border-gray-400  border-2 w-[100px] rounded-lg`}
+        to={'/?mode=top'}
+        onClick={() => setMode('top')}
       >
         Top Rated
       </Link>
-      {/* </button> */}
     </div>
   );
 };
