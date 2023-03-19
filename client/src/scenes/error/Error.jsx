ae5ebb93739de62eb3afee88d294e9775996277a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-03-18 20:26:19 -0400

    Client: Updated Error Page
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- client/src/scenes/error/Error.jsx
+++ client/src/scenes/error/Error.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 import PageContent from '../../components/PageContent';
 import Navbar from '../../components/Navbar';
@@ -20,12 +20,35 @@ const ErrorPage = () => {
   }
 
   return (
-    <div className='w-full mx-auto pt-[120px] sm:pt-24'>
+    <>
       <Navbar />
-      <PageContent title={title}>
-        <p>{message}</p>
-      </PageContent>
-    </div>
+      <main>
+        <div
+          className='pt-[115px] sm:pt-[63px] flex flex-col justify-center items-center
+          h-[calc(100vh-115px)] sm:h-[calc(100vh-63px)] '
+        >
+          <PageContent title={title}>
+            <p>{message}</p>
+          </PageContent>
+          <div className='pt-4'>
+            <Link
+              className='bg-gradient-blue hover:bg-gradient-blue w-full
+              min-w-[200px] mx-auto text-gray-800 hover:text-white shadow
+              font-semibold py-2 px-4 rounded-[20px]'
+              to={'/'}
+            >
+              Go Home
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+    // <div className='w-full mx-auto pt-[120px] sm:pt-24'>
+    //   <Navbar />
+    //   <PageContent title={title}>
+    //     <p>{message}</p>
+    //   </PageContent>
+    // </div>
   );
 };
 
